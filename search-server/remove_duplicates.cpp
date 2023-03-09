0126025a51f58627d841611ee9c62b7ea8cdad13
igor pankin <117118168+igorkiy@users.noreply.github.com>
2023-03-09 11:29:43 +0400

    changed

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -1,24 +1,42 @@
-#include "remove_duplicates.h";
-#include <iostream>;
-#include<map>;
-#include "log_duration.h"
-using namespace std;
+#include "search_server.h"
+#include "remove_duplicates.h"
+#include "document.h"
+#include "string_processing.h"
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+#include <deque>
+#include <numeric>
+
 
 void RemoveDuplicates(SearchServer& search_server) {
-	std::vector<pair <int, std::map<string, double>>> documents;
-	for (auto doc : search_server) {
-		documents.push_back({ doc, search_server.GetWordFrequencies(doc) });
-	}
-		
-	for ( auto it = documents.begin(); it != documents.end(); it = next(it)) { 
-		for (auto next_it = next(it); next_it != documents.end();) {
-			if (next_it->first > it->first && next_it->second == it->second) {
-				cout << "Found duplicate document id " << next_it->first << endl;
-				search_server.RemoveDocument(next_it->first);
-				next_it = documents.erase(next_it);
-			}
-			else next_it = next(next_it);
+	std::set<std::set<std::string>> all_document; // структура слов для всех документов по id
+	std::set<int> to_erase;
+	for (const int document_id : search_server) {
+		std::set<std::string> key_words; // только слова документа без id
+		std::map<std::string, double> id_freq = search_server.GetWordFrequencies(document_id); // получаю частоту слов для id
+		// записываю только слова для id В вектор
+		for (auto&& [first, second] : id_freq) {
+			key_words.insert(first);
 		}
-
+		// добавляю set (слов) в итогов document_struct при услови и что аналогичного набора там нет 
+		if (all_document_struct.count(key_words) == 0) {
+			all_document_struct.insert(key_words);
+		}
+		else {
+			std::cout << "Found duplicate document id " << document_id << std::endl;
+			to_erase.insert(document_id);
+		}
 	}
+
+	for (auto document_id : to_erase) {
+		search_server.RemoveDocument(document_id);  // удаляю документ)
+	}
+
 }
+

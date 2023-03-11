b986702f2d96be09fb644be3bb7d5fc3e97de1ba
igor pankin <117118168+igorkiy@users.noreply.github.com>
2023-03-12 03:26:38 +0400

    tests

---
+++ yandex_practicum/8sprint/main.cpp
@@ -0,0 +1,21 @@
+#include <cassert>
+#include <string>
+#include <string_view>
+#include "Translator.h"
+
+using std::string;
+
+void TestSimple() {
+    Translator translator;
+    translator.Add(string("okno"s), string("window"s));
+    translator.Add(string("stol"s), string("table"s));
+
+    assert(translator.TranslateForward("okno"s) == "window"s);
+    assert(translator.TranslateBackward("table"s) == "stol"s);
+    assert(translator.TranslateForward("table"s) == ""s);
+}
+
+int main() {
+    TestSimple();
+    return 0;
+}

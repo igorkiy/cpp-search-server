d23feddad3e23285825ad23c425d7e2371d91098
igor pankin <117118168+igorkiy@users.noreply.github.com>
2023-03-12 03:24:49 +0400

    translator

---
+++ yandex_practicum/8sprint/translator.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <set>
+#include <string_view>
+#include <map>
+#include<deque>
+using namespace std;
+
+class Translator {
+public:
+    Translator() = default;
+    void Add(string_view source, string_view target);
+    string_view TranslateForward(string_view source) const;
+    string_view TranslateBackward(string_view target) const;
+
+private:
+    deque<string> wordsStore_{};
+    map<string_view, string_view> forwardMap_; 
+    map<string_view, string_view> backwardMap_;
+
+};
